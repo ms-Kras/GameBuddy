7435e1210a6d9b42e0fbfb8927e323340e87af27
kef <kekekefir@gmail.com>
2023-07-13 18:12:17 +0300

    getprofile upd

--- notebooks/getprofile.py
+++ notebooks/getprofile.py
@@ -6,6 +6,6 @@ import re
 def get_profile_id(url):
     userpage = requests.get(url)
     user_page = BeautifulSoup(userpage.content, 'lxml')
-    steamid = user_page.find('div', class_="commentthread_area")['id']
-    steamid = ''.join([symbol for symbol in steamid if symbol if not re.findall("(?i)([a-z]+)", symbol) and symbol not in ['_']])
+    text = str(user_page.find('div', class_="responsive_page_template_content"))
+    steamid = int(text[text.find('"steamid"')+11:text.find('"steamid"')+28])
     return steamid
